1630811393df56ca19d2b52d8507dda595e942e4
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-02-05 21:38:09 +0300

    introduction to Arrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -5,4 +5,21 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
+	cout << "Hello Arrays" << endl;
+	const int SIZE = 5;
+	int arr[SIZE];
+
+	cout << "¬ведите " << SIZE << " целые числа: " << endl;
+	//arr[2] = 2048; // обращение к элементу массива на запись
+	//cout << arr[2] << endl; // обращение к элементу массива на чтение
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << " Ёлемент номер " << i << " : ";
+		cin >> arr[i];
+	}
+	cout << " ¬ывод элементов массива: " << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << " ";
+	}
 }
