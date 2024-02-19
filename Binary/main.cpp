19b908b666404dee4eb47b25ee49811db5688621
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-02-19 21:25:09 +0300

    Binary encoding

--- Binary/main.cpp
+++ Binary/main.cpp
@@ -8,28 +8,20 @@ using std::endl;
 void main()
 {
 	setlocale(LC_ALL, "");
-    int celoe;
-    cout << "Введите целое число: ";
-    cin >> celoe;
-    int binary[10];
-    int ind = 0;
-
-    if (celoe == 0) 
+    int decimal;
+    cout << "Введите целое число: ", cin >> decimal;
+    const int n = 32;
+    bool bin[n] = {};
+    int i = 0;
+    while (decimal > 0)
     {
-        cout << "Число " << celoe << " в двоичной системе: " << endl;
+        bin[i] = decimal % 2;
+        i++;
+        decimal /= 2;
     }
-    else {
-        while (celoe > 0) 
-        {
-            binary[ind] = celoe % 2;
-            celoe = celoe / 2;
-            ind++;
-        }
-        cout << "Число в двоичной системе: ";
-        for (int i = ind - 1; i >= 0; i--) 
-        {
-            cout << binary[i];
-        }
-        cout << endl;
+    for (--i; i >= 0; i--)
+    {
+        cout << bin[i];
     }
+    cout << endl;
 }
