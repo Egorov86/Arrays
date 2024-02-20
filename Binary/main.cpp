5a8594763f654d724fe16bf725b57c8d876a4d36
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-02-20 19:51:05 +0300

    Binary encoding

--- Binary/main.cpp
+++ Binary/main.cpp
@@ -12,6 +12,7 @@ void main()
     cout << "Введите целое число: ", cin >> decimal;
     const int n = 32;
     bool bin[n] = {};
+#ifdef VAR_1
     int i = 0;
     while (decimal > 0)
     {
@@ -19,6 +20,10 @@ void main()
         i++;
         decimal /= 2;
     }
+#endif // VAR_1
+
+    int i = 0;
+    for(; decimal; bin[i++] = decimal % 2, decimal /2);
     for (--i; i >= 0; i--)
     {
         cout << bin[i];
